98dd8a60b7a0c01489104c76c40d197cf0415dc3
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-14 01:00:45 -0700

    commit tree 639515a89b1ea1b2253d68429753c66d170e6dd8

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = num1;
+const num4 = num2;
+const mod = num1 % num2;
+
+const max = Math.max(num1, num2, 20);
